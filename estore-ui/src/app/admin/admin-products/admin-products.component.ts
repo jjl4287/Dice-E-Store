258553cd6a12d16d7114d1547c7d789eda3e48a1
Jakob Langtry <jjl4287@g.rit.edu>
2022-10-19 17:15:54 -0400

    added product search to ts

--- estore-ui/src/app/admin/admin-products/admin-products.component.ts
+++ estore-ui/src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/product';
 import { ProductService } from 'src/app/product.service';
+import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-products',
@@ -16,6 +20,16 @@ export class AdminProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getProducts();
+    this.products$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(200),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.productService.searchProducts(term)),
+    );
   }
 
   // using the service from the backend to properly grab products
@@ -29,4 +43,13 @@ export class AdminProductsComponent implements OnInit {
     this.products = this.products.filter((h: Product) => h !== product);
     this.productService.deleteProduct(product.id).subscribe();
   }
+
+  products$!: Observable<Product[]>;
+  private searchTerms = new Subject<string>();
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
 }
