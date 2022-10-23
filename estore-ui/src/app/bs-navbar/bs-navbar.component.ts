647d470f67c23be9beda4d57ce3c7740a9114b5e
Chris Ferioli <cjf4592@rit.edu>
2022-10-22 20:51:40 -0400

    added navbar search implementation

--- estore-ui/src/app/bs-navbar/bs-navbar.component.ts
+++ estore-ui/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/product';
+import { ProductService } from 'src/app/product.service';
+import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'bs-navbar',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent implements OnInit {
+  products: any;
+  products$!: Observable<Product[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
+    this.products = productService.getProducts();
   }
 
+  ngOnInit(): void {
+    this.productService.getProducts()
+      .subscribe(products => this.products = products);
+    this.products$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(200),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.productService.searchProducts(term)),
+      );
+  }
+  
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
