3c95e9b524450983b778aa62db53d48d6c45053f
Chris Ferioli <cjf4592@rit.edu>
2022-10-22 21:00:59 -0400

    added search implementation

--- estore-ui/src/app/products/products.component.ts
+++ estore-ui/src/app/products/products.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -10,9 +14,23 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent implements OnInit {
   products: any;
+  products$!: Observable<Product[]>;
+  private searchTerms = new Subject<string>();
 
   constructor(private productService: ProductService) { 
     this.products = productService.getProducts();
+    this.productService.getProducts()
+      .subscribe(products => this.products = products);
+    this.products$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(200),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.productService.searchProducts(term)),
+    );
   }
 
   ngOnInit(): void {
@@ -35,10 +53,9 @@ export class ProductsComponent implements OnInit {
       });
   }
 
-  // using service to delete products
-  delete(product: Product): void {
-    this.products = this.products.filter((h: Product) => h !== product);
-    this.productService.deleteProduct(product.id).subscribe();
-  }
 
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
